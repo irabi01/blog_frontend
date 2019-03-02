89d210a471e515e5d256651b4db0428007e5315a
irabi <irabikassim01@gmail.com>
2019-03-02 12:43:15 +0300

    create three columns on the home.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,26 @@ import React, { Component } from 'react'
 export class Home extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="home_container">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="other_issues">
+                  <h1>Display Something here</h1>
+                </div>
+              </div>
+              <div className="col-md-5">
+                <div className="other_issues">
+                  <h1>Blog Content here</h1>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="other_issues">
+                  <h1>Display Advertisement</h1>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
