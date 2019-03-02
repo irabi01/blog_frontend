eb8b486745aa5252a53899658cd9880b582a5796
irabi <irabikassim01@gmail.com>
2019-03-02 15:26:58 +0300

    fixed navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ export class Navigation extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg fixed-top navigation">
             <Link className="navbar-brand" to="/">Logo Here</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
