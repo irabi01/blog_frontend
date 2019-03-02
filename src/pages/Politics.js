39feb214388b15fd7a4f7d7fd9510d4ccd1e640d
irabi <irabikassim01@gmail.com>
2019-03-02 12:36:05 +0300

    add three file to the pages directory(sports, education and politics)

---
+++ src/pages/Politics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Politics extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Politics
