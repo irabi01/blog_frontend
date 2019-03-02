94f8a2705143c677ef9de830fc9a8442815cde79
irabi <irabikassim01@gmail.com>
2019-03-02 12:32:13 +0300

    add / to the navibar-brand Link in Navigation.js file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ export class Navigation extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="#">Logo Here</Link>
+            <Link className="navbar-brand" to="/">Logo Here</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
