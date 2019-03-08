fab56e268de56500253512b9b404cda8bde39cad
irabi <irabikassim01@gmail.com>
2019-03-08 12:54:32 +0300

    add link to login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,7 +23,7 @@ export class Login extends Component {
                             </form>
                         </div>
                         <div className="go_to_sign_up">
-                            <p>Don't have an account? <Link to="" className="link">Sign up</Link> </p>
+                            <p>Don't have an account? <Link to="/auth/register" className="link">Sign up</Link> </p>
                         </div>
                     </div>
                 </div>
