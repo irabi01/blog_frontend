31b35566618573050bad16999872cebc6d0d54c7
irabi <irabikassim01@gmail.com>
2019-03-08 15:16:16 +0300

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <p><i className="fa fa-copyright"></i> 2019. All rights reserved. CheersApp</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer
