44a5b09e05dbb8bd27258d6f21a3212ee6e9ce7a
irabi <irabikassim01@gmail.com>
2019-03-08 12:43:37 +0300

    add form tags

---
+++ src/pages/Register.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+  render() {
+    return (
+      <div className="register_container">
+        <div className="row">
+            <div className="col-md-8">
+                <div className="images_part">
+                    <h1>Irabi kassim</h1>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="register_part">
+                    <h1>Cheers</h1>
+                    <form> 
+                        <div className="form-group">
+                            <input type="text" name="fullname" id="fullname" placeholder="Full Name"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" name="username" id="username" placeholder="Phone number, username, or email"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="password" name="password" id="password" placeholder="Password"/>
+                        </div>
+                        <div className="form-group">
+                            <button>Sign up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register
